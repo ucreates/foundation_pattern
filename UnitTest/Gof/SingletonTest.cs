d944eb5cdf5fbcfed5624809812f74eb149f38fa
ucreates <ucreates.inc@gmail.com>
2025-08-16 19:05:19 +0900

    add gof/singleton pattern.

---
+++ UnitTest/Gof/SingletonTest.cs
@@ -0,0 +1,17 @@
+﻿using Pattern.Gof;
+
+namespace UnitTest.Gof;
+
+public class SingletonTest
+{
+    [Test]
+    public void InstanceTest()
+    {
+        Assert.That(Singleton.Instance, Is.Null);
+        Singleton.GetInstance().Execute();
+        Assert.That(Singleton.Instance, Is.Not.Null);
+        var instance1 = Singleton.GetInstance();
+        var instance2 = Singleton.GetInstance();
+        Assert.That(instance1, Is.SameAs(instance2));
+    }
+}
